e663cfb87a42d2b63864a1ce608c963d48e2cfcf
DamienLoisel <damien.sti2D@gmail.com>
2024-12-13 20:47:58 +0100

    Contrôles du jeu changé pour que cela soit jouable sur qwerty

--- src/hooks/hooks.c
+++ src/hooks/hooks.c
@@ -5,72 +5,72 @@
 /* Gestion des touches */
 int key_press(int keycode, t_game *game)
 {
-    double old_dir_x;
-    double old_plane_x;
+	double old_dir_x;
+	double old_plane_x;
 
-    if (keycode == 65307) /* ESC */
-        exit(0);
-    else if (keycode == 122) /* Z - Avancer */
-    {
-        if (game->map.fullmap[(int)(game->player.pos_x + game->player.dir_x * MOVE_SPEED)]
-                     [(int)game->player.pos_y] != '1')
-            game->player.pos_x += game->player.dir_x * MOVE_SPEED;
-        if (game->map.fullmap[(int)game->player.pos_x]
-                     [(int)(game->player.pos_y + game->player.dir_y * MOVE_SPEED)] != '1')
-            game->player.pos_y += game->player.dir_y * MOVE_SPEED;
-    }
-    else if (keycode == 115) /* S - Reculer */
-    {
-        if (game->map.fullmap[(int)(game->player.pos_x - game->player.dir_x * MOVE_SPEED)]
-                     [(int)game->player.pos_y] != '1')
-            game->player.pos_x -= game->player.dir_x * MOVE_SPEED;
-        if (game->map.fullmap[(int)game->player.pos_x]
-                     [(int)(game->player.pos_y - game->player.dir_y * MOVE_SPEED)] != '1')
-            game->player.pos_y -= game->player.dir_y * MOVE_SPEED;
-    }
-    else if (keycode == 100) /* Q - Pivot gauche */
-    {
-        old_dir_x = game->player.dir_x;
-        game->player.dir_x = game->player.dir_x * cos(-ROTATION_SPEED)
-                          - game->player.dir_y * sin(-ROTATION_SPEED);
-        game->player.dir_y = old_dir_x * sin(-ROTATION_SPEED)
-                          + game->player.dir_y * cos(-ROTATION_SPEED);
-        old_plane_x = game->player.plane_x;
-        game->player.plane_x = game->player.plane_x * cos(-ROTATION_SPEED)
-                            - game->player.plane_y * sin(-ROTATION_SPEED);
-        game->player.plane_y = old_plane_x * sin(-ROTATION_SPEED)
-                            + game->player.plane_y * cos(-ROTATION_SPEED);
-    }
-    else if (keycode == 113) /* D - Pivot droite */
-    {
-        old_dir_x = game->player.dir_x;
-        game->player.dir_x = game->player.dir_x * cos(ROTATION_SPEED)
-                          - game->player.dir_y * sin(ROTATION_SPEED);
-        game->player.dir_y = old_dir_x * sin(ROTATION_SPEED)
-                          + game->player.dir_y * cos(ROTATION_SPEED);
-        old_plane_x = game->player.plane_x;
-        game->player.plane_x = game->player.plane_x * cos(ROTATION_SPEED)
-                            - game->player.plane_y * sin(ROTATION_SPEED);
-        game->player.plane_y = old_plane_x * sin(ROTATION_SPEED)
-                            + game->player.plane_y * cos(ROTATION_SPEED);
-    }
-    else if (keycode == 65361) /* Flèche droite */
-    {
-        if (game->map.fullmap[(int)(game->player.pos_x)]
-                     [(int)(game->player.pos_y + game->player.dir_x * MOVE_SPEED)] != '1')
-            game->player.pos_y += game->player.dir_x * MOVE_SPEED;
-        if (game->map.fullmap[(int)(game->player.pos_x - game->player.dir_y * MOVE_SPEED)]
-                     [(int)game->player.pos_y] != '1')
-            game->player.pos_x -= game->player.dir_y * MOVE_SPEED;
-    }
-    else if (keycode == 65363) /* Flèche gauche */
-    {
-        if (game->map.fullmap[(int)(game->player.pos_x)]
-                     [(int)(game->player.pos_y - game->player.dir_x * MOVE_SPEED)] != '1')
-            game->player.pos_y -= game->player.dir_x * MOVE_SPEED;
-        if (game->map.fullmap[(int)(game->player.pos_x + game->player.dir_y * MOVE_SPEED)]
-                     [(int)game->player.pos_y] != '1')
-            game->player.pos_x += game->player.dir_y * MOVE_SPEED;
-    }
-    return (0);
+	if (keycode == 65307) /* ESC */
+		close_window(game);
+	else if (keycode == 119) /* W - Avancer */
+	{
+		if (game->map.fullmap[(int)(game->player.pos_x + game->player.dir_x * MOVE_SPEED)]
+					 [(int)game->player.pos_y] != '1')
+			game->player.pos_x += game->player.dir_x * MOVE_SPEED;
+		if (game->map.fullmap[(int)game->player.pos_x]
+					 [(int)(game->player.pos_y + game->player.dir_y * MOVE_SPEED)] != '1')
+			game->player.pos_y += game->player.dir_y * MOVE_SPEED;
+	}
+	else if (keycode == 115) /* S - Reculer */
+	{
+		if (game->map.fullmap[(int)(game->player.pos_x - game->player.dir_x * MOVE_SPEED)]
+					 [(int)game->player.pos_y] != '1')
+			game->player.pos_x -= game->player.dir_x * MOVE_SPEED;
+		if (game->map.fullmap[(int)game->player.pos_x]
+					 [(int)(game->player.pos_y - game->player.dir_y * MOVE_SPEED)] != '1')
+			game->player.pos_y -= game->player.dir_y * MOVE_SPEED;
+	}
+	else if (keycode == 100) /* D - Pivot droit */
+	{
+		old_dir_x = game->player.dir_x;
+		game->player.dir_x = game->player.dir_x * cos(-ROTATION_SPEED)
+						  - game->player.dir_y * sin(-ROTATION_SPEED);
+		game->player.dir_y = old_dir_x * sin(-ROTATION_SPEED)
+						  + game->player.dir_y * cos(-ROTATION_SPEED);
+		old_plane_x = game->player.plane_x;
+		game->player.plane_x = game->player.plane_x * cos(-ROTATION_SPEED)
+							- game->player.plane_y * sin(-ROTATION_SPEED);
+		game->player.plane_y = old_plane_x * sin(-ROTATION_SPEED)
+							+ game->player.plane_y * cos(-ROTATION_SPEED);
+	}
+	else if (keycode == 97) /* A - Pivot gauche */
+	{
+		old_dir_x = game->player.dir_x;
+		game->player.dir_x = game->player.dir_x * cos(ROTATION_SPEED)
+						  - game->player.dir_y * sin(ROTATION_SPEED);
+		game->player.dir_y = old_dir_x * sin(ROTATION_SPEED)
+						  + game->player.dir_y * cos(ROTATION_SPEED);
+		old_plane_x = game->player.plane_x;
+		game->player.plane_x = game->player.plane_x * cos(ROTATION_SPEED)
+							- game->player.plane_y * sin(ROTATION_SPEED);
+		game->player.plane_y = old_plane_x * sin(ROTATION_SPEED)
+							+ game->player.plane_y * cos(ROTATION_SPEED);
+	}
+	else if (keycode == 65361) /* Flèche droite */
+	{
+		if (game->map.fullmap[(int)(game->player.pos_x)]
+					 [(int)(game->player.pos_y + game->player.dir_x * MOVE_SPEED)] != '1')
+			game->player.pos_y += game->player.dir_x * MOVE_SPEED;
+		if (game->map.fullmap[(int)(game->player.pos_x - game->player.dir_y * MOVE_SPEED)]
+					 [(int)game->player.pos_y] != '1')
+			game->player.pos_x -= game->player.dir_y * MOVE_SPEED;
+	}
+	else if (keycode == 65363) /* Flèche gauche */
+	{
+		if (game->map.fullmap[(int)(game->player.pos_x)]
+					 [(int)(game->player.pos_y - game->player.dir_x * MOVE_SPEED)] != '1')
+			game->player.pos_y -= game->player.dir_x * MOVE_SPEED;
+		if (game->map.fullmap[(int)(game->player.pos_x + game->player.dir_y * MOVE_SPEED)]
+					 [(int)game->player.pos_y] != '1')
+			game->player.pos_x += game->player.dir_y * MOVE_SPEED;
+	}
+	return (0);
 }
