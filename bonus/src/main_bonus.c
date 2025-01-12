e4f5a5dd4dc573565db0c0b20da878eee5969d4c
Damien Loisel <dloisel@paul-f4Br7s6.clusters.42paris.fr>
2025-01-12 01:09:53 +0100

    Ecran des contrôles rajouté

--- bonus/src/main_bonus.c
+++ bonus/src/main_bonus.c
@@ -6,11 +6,38 @@
 /*   By: dloisel <dloisel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 07:24:01 by dloisel           #+#    #+#             */
-/*   Updated: 2025/01/11 01:21:44 by dloisel          ###   ########.fr       */
+/*   Updated: 2025/01/12 01:08:38 by dloisel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d_bonus.h"
+
+void	display_victory_screen(void)
+{
+	printf("\n\n");
+	printf(GREEN"    🌟 ⭐️ ✨ CONGRATULATIONS! ✨ ⭐️ 🌟\n");
+	printf("     ═══════════════════════════════\n");
+	printf("           You have completed\n");
+	printf("       	   the amazing maze!\n\n");
+	printf("      Thanks for playing this game!\n");
+	printf("     ═══════════════════════════════\n"RESET);
+	printf("\n\n");
+}
+
+void	display_control(void)
+{
+	ft_printf("\n");
+	ft_printf(YELLOW"Welcome to cub3d by double D\n"RESET);
+	ft_printf("\n");
+	ft_printf(BLUE"Here are the controls: \n");
+	ft_printf(" - < WASD > --> To move the character.\n");
+	ft_printf(" - < E > --> Open doors and interact with the WIN tile.\n");
+	ft_printf(" - Left/Right arrows --> Allows you to move the camera.\n");
+	ft_printf(" - The mouse allows you to control the camera,\n"RESET);
+	ft_printf("\n");
+	ft_printf(YELLOW"The goal of the game is to find the WINDOWS logo.\n");
+	ft_printf("Good luck!!!\n"RESET);
+}
 
 /* Point d'entrée */
 int	main(int argc, char **argv)
@@ -25,6 +52,7 @@ int	main(int argc, char **argv)
 		cleanup_game(&game);
 		return (1);
 	}
+	display_control();
 	mlx_hook(game.win, 2, 1L << 0, key_press, &game);
 	mlx_hook(game.win, 3, 1L << 1, key_release, &game);
 	mlx_hook(game.win, 17, 0, close_window, &game);
@@ -32,16 +60,4 @@ int	main(int argc, char **argv)
 	mlx_loop_hook(game.mlx, render, &game);
 	mlx_loop(game.mlx);
 	return (0);
-}
-
-void	display_victory_screen(void)
-{
-	printf("\n\n");
-	printf("    🌟 ⭐️ ✨ CONGRATULATIONS! ✨ ⭐️ 🌟\n");
-	printf("    ═══════════════════════════════\n");
-	printf("         You have completed\n");
-	printf("        the amazing maze!\n\n");
-	printf("    Thanks for playing this game!\n");
-	printf("    ═══════════════════════════════\n");
-	printf("\n\n");
 }
